0a9862c7dc9bdea8a0d85fa4b986fcaea0bbc28f
Eric Kwok <46385457+Kwok-Eric-216397150@users.noreply.github.com>
2020-02-15 12:58:23 -0500

    Delete db.py

--- db.py
+++
@@ -1,8 +0,0 @@
-from pymongo import MongoClient
-
-client = MongoClient("mongodb+srv://HireIO:TestPDFfile@cluster0-1god2.mongodb.net/test?retryWrites=true&w=majority")
-
-db = client.get_database('HireIO_db')
-
-records = db.HireIO_Records
-
